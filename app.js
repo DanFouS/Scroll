1315a2aac232d1fdadc1568b0724791c5b218cf3
DanFouS <rouzmfawer@gmail.com>
2021-10-11 16:44:09 +0200

    close links1

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
 // Close Links
+const navToggle = document.querySelector(".navToggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  //   console.log(containerHeight);
+  const linksHeight = links.getBoundingClientRect().height;
+  //   console.log(linksheight);
+});
 
 // Fixed Navbar
 
