e2ecd6dcd6e07ce744c08679d7adb57398c4bded
DanFouS <rouzmfawer@gmail.com>
2021-10-13 23:16:10 +0200

    .

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const scrollLinks = document.querySelectorAll(".scroll-link");
 scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     // prevent default
-
+    console.log(e);
     e.preventDefault();
 
     // navigate to specific spot
