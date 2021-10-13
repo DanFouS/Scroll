9f7c94356461356f42b4e35d4237d6ff8960cbc2
DanFouS <rouzmfawer@gmail.com>
2021-10-13 23:15:42 +0200

    .

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ navToggle.addEventListener("click", function () {
 });
 const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
-fixed navbar
+// fixed navbar
 window.addEventListener("scroll", function () {
   const scrollHeight = window.pageYOffset;
   const navHeight = navbar.getBoundingClientRect().height;
